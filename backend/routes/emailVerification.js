7f1096546ec11c37661350bf0bec9c4731af9e04
PrasoonRawat <prasoon818@gmail.com>
2025-03-11 09:44:28 +0530

    adde confirmation email

--- backend/routes/emailVerification.js
+++ backend/routes/emailVerification.js
@@ -1,7 +1,10 @@
 import express from "express";
 import Subscriber from "../models/Subscriber.js";
 import PendingSubscriber from "../models/PendingSubscriber.js";
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const router = express.Router();
 
 router.get("/:token", async (req, res) => {
@@ -11,6 +14,10 @@ router.get("/:token", async (req, res) => {
     try {
         // Find pending subscriber
         const pendingSub = await PendingSubscriber.findOne({ token });
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS }
+        });
 
         if (!pendingSub) {
             return res.status(400).json({ error: "Invalid or expired token." });
@@ -20,7 +27,17 @@ router.get("/:token", async (req, res) => {
         await Subscriber.create({ emailOrPhone: pendingSub.email, states: pendingSub.states });
         
         // Remove from pending subscribers
+        await transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to: pendingSub.email,
+            subject: "Subscription Confirmation",
+            text: "Congratulations you have been successfully subscribed to our notification service."
+        });
+        
         await PendingSubscriber.deleteOne({ _id: pendingSub._id });
+
+
+
         return res.json({ message: "Subscription confirmed successfully!" });
 
 
