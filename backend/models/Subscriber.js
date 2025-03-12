de791aa2cdaaed1708dcaf02ca38a7937310272f
PrasoonRawat <prasoon818@gmail.com>
2025-03-12 14:02:06 +0530

    added count and paymentstatus

--- backend/models/Subscriber.js
+++ backend/models/Subscriber.js
@@ -12,11 +12,14 @@
 
 
 import mongoose from "mongoose";
+import { type } from "os";
 
 const SubscriberSchema = new mongoose.Schema({
     emailOrPhone: { type: String, required: true, unique: true },
     states: { type: [String], required: true },
-    subscribedAt: { type: Date, default: Date.now }
+    subscribedAt: { type: Date, default: Date.now, expire:  3153600},
+    count: { type: Number, default: 4 },
+    paymentstatus: {type: Boolean, default: false},
 });
 
 export default mongoose.model("Subscriber", SubscriberSchema);
